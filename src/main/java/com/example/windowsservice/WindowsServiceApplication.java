2f98bc6be125174390a241d689f8d182964a2b28
Nate Hall <nate.hall@insight.com>
2021-03-16 14:54:17 -0400

    initial commit

---
+++ src/main/java/com/example/windowsservice/WindowsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.windowsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindowsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/windowsservice/WindowsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.windowsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
